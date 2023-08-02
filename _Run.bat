5b6f882ab1b7452897d456b3d15a8445164cf246
budul100 <MarekGrochowski@web.de>
2023-08-02 18:05:39 -0400

    Script updated

--- _Run.bat
+++ _Run.bat
@@ -2,11 +2,10 @@
 
 set /p "INPUTFILE=Enter input path: "
 
-for %%f in ("%INPUTFILE%") do set FEATURESFILE=%%~dpnf_Merged.geojson
-for %%f in ("%INPUTFILE%") do set ROUTINGFILE=%%~dpnf_Lines.txt
+for %%f in ("%INPUTFILE%") do set FEATURESFILE=%%~dpnf_.geojson
 
 echo.
 echo Simplify %INPUTFILE%
 echo.
 
-.\SFASimplifierCLI\bin\Debug\net6.0\SFASimplifierCLI.exe -i "%INPUTFILE%" -f "%FEATURESFILE%" -r "%ROUTINGFILE%"
+.\SFASimplifierCLI\bin\Debug\net6.0\SFASimplifierCLI.exe -i "%INPUTFILE%" -o "%FEATURESFILE%"
