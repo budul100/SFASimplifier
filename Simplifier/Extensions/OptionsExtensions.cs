86b30882d338e9663cb121199e12825c0b36dda4
budul100 <MarekGrochowski@web.de>
2023-08-18 14:11:00 -0400

    Minor correction

--- Simplifier/Extensions/OptionsExtensions.cs
+++ Simplifier/Extensions/OptionsExtensions.cs
@@ -30,8 +30,8 @@ namespace SFASimplifier.Simplifier.Extensions
 
                 result = new Envelope(
                     x1: x1,
-                    x2: y1,
-                    y1: x2,
+                    x2: x2,
+                    y1: y1,
                     y2: y2);
             }
 
