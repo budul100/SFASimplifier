a0ec6dbec1e14c2378e69de18f5e5fbecb1eb2ac
budul100 <MarekGrochowski@web.de>
2023-08-20 22:29:28 -0400

    Build script updated

--- _Build.bat
+++ _Build.bat
@@ -1,5 +1,10 @@
 @echo off
 
+SET OutputDir1=%USERPROFILE%\OneDrive - IVU Traffic Technologies AG\Geodata\SFASimplifier
+SET OutputDir2=\\ivu-ag.com\storage\tausch-bln\mgr\Geodata\SFASimplifier
+
+SET DeploymentPath=.\_Publish
+
 echo.
 echo ### Clean up ###
 echo.
@@ -17,3 +22,10 @@ echo.
 dotnet publish CLI -p:DebugType=None -p:DebugSymbols=false -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishReadyToRun=true -p:PublishSingleFile=true --self-contained --configuration Release --runtime win-x64 --output _Publish
 
 rem dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
+
+echo.
+echo ### Deploy application ###
+echo.
+
+copy /y "%DeploymentPath%\*.*" "%OutputDir1%\"
+copy /y "%DeploymentPath%\*.*" "%OutputDir2%\"
